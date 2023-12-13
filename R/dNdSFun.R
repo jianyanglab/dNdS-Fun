9ad08a5e58a69c48eeb61dbd471ff7fd1ddfac5f
Sandy <2302853160@qq.com>
2023-12-13 16:19:49 +0800

    6_test

--- R/dNdSFun.R
+++ R/dNdSFun.R
@@ -1,4 +1,4 @@
-#' @keywords internal
+
 dNdSFun <- function(mutsFile,refDb_element, reg, positive, negative, positiveThreshold, negativeThreshold, gene_group, globaldnds_outFile,
                  genelevel_selloc_outFile, genelevel_selcv_outFile, iscv, score = "ture", model = 3, negmu = 1){
     rm(list=ls())
